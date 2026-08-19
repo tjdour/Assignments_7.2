e14d8c30670b1e0d13220836696298d3a447b799
tjdour <tongjdour@outlook.com>
2026-08-18 21:01:11 -0400

    Assignment 7.2.2

---
+++ Assignment_7.2.2/Program.cs
@@ -0,0 +1,60 @@
+﻿//Assignment 7.2.2
+//Given a string s, reverse only all the vowels in the string and return it.
+
+string vowels = "aeiou";
+
+string s = "hello";
+Console.WriteLine(ReverseVowels(s));
+
+s = "avacado";
+Console.WriteLine(ReverseVowels(s));
+
+s = "intelligent";
+Console.WriteLine(ReverseVowels(s));
+
+
+string ReverseVowels(string s)
+{
+    char[] letters = s.ToCharArray();
+
+    int left = 0;
+    int right = letters.Length - 1;
+
+    while (left < right)
+    {
+        while (left < right && !IsVowel(letters[left]))
+        {
+            left++;
+        }
+
+        while (left < right && !IsVowel(letters[right]))
+        {
+            right--;
+        }
+
+        char temp = letters[left];
+        letters[left] = letters[right];
+        letters[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    return new string(letters);
+}
+
+
+bool IsVowel(char letter)
+{
+    letter = char.ToLower(letter);
+
+    for (int i = 0; i < vowels.Length; i++)
+    {
+        if (letter == vowels[i])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
